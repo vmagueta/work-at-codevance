fcfc12ec7109f39fcf82364e5f9dc8bcd616ffad
vmagueta <solermvictor@gmail.com>
2024-12-16 12:16:12 +0000

    added procfile

--- core/settings.py
+++ core/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = (
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Celery configuration
 CELERY_BROKER_URL = "redis://localhost:6379/0"
